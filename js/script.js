e53cd30f2bcc7bc68f13c35cdee1580d5f7d1aa7
PauloAlves8039 <pj38alves@gmail.com>
2023-10-24 15:40:06 -0300

    Aplicando melhoria na confirmação de contato

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault();
     window.location.href = "formulario-enviado.html";
 });
+
+document.querySelector(".button-back").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
